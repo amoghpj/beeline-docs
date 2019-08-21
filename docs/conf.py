e7dfa5c4d1305e07961626dba3afca5c70ed9c36
Amogh Jalihal <amogh.jalihal@gmail.com>
2019-08-21 14:15:48 -0400

    Fix conf

--- docs/conf.py
+++ docs/conf.py
@@ -206,5 +206,3 @@ epub_exclude_files = ['search.html']
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-import matplotlib
-matplotlib.use('agg')
